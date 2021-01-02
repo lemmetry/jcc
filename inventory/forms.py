9900ddf49a6dd71d0ba7781c5936f9087d8747ea
Artem Kisel <artemkisel@yahoo.com>
2021-01-01 19:27:39 -0500

    Removed code that is unused in current state of order form development.

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,12 +1,1 @@
 from django import forms
-import datetime
-
-
-class OrderForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(OrderForm, self).__init__(*args, **kwargs)
-        for i in range(1, 100):
-            self.fields['order_' + str(i)] = forms.CharField()
-
-
-form = OrderForm()
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 
-from .forms import OrderForm
 from inventory.models import Bag
 from inventory.models import BagCompartmentToItemAssociation
 from inventory.models import KitToItemAssociation
@@ -9,47 +7,39 @@ from inventory.models import KitCompartmentToItemAssociation
 
 
 def order_form(request):
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('/')
-    else:
-        form = OrderForm()
+    red_bag = Bag.objects.get(name='Red Bag "Stat Pack"')
+    vehicle_bags = [red_bag]
 
-        red_bag = Bag.objects.get(name='Red Bag "Stat Pack"')
-        vehicle_bags = [red_bag]
+    r = request.GET
+    for key, value in r.items():
+        try:
+            value = int(value)
+            if value > 0:
+                association_class_name = key.split('_')[0]
+                association_object_pk = key.split('_')[1]
+                # ^^^ association_class_name and association_pk will provide enough information about the ordered
+                # item, including it's location.
 
-        r = request.GET
-        for key, value in r.items():
-            try:
-                value = int(value)
-                if value > 0:
-                    association_class_name = key.split('_')[0]
-                    association_object_pk = key.split('_')[1]
-                    # ^^^ association_class_name and association_pk will provide enough information about the ordered
-                    # item, including it's location.
+                if association_class_name == 'bagcompartmenttoitemassociation':
+                    bag_compartment_to_item_association = BagCompartmentToItemAssociation.objects.get(pk=association_object_pk)
+                    item = bag_compartment_to_item_association.item
+                    print('%s in %s' % (item.name, bag_compartment_to_item_association.bag_compartment.name))
+                elif association_class_name == 'kittoitemassociation':
+                    kit_to_item_association = KitToItemAssociation.objects.get(pk=association_object_pk)
+                    item = kit_to_item_association.item
+                    print('%s in %s' % (item.name, kit_to_item_association.kit.name))
+                elif association_class_name == 'kitcompartmenttoitemassociation':
+                    kit_compartment_to_item_association = KitCompartmentToItemAssociation.objects.get(pk=association_object_pk)
+                    item = kit_compartment_to_item_association.item
+                    print('%s in %s' % (item.name, kit_compartment_to_item_association.kit_compartment.name))
+                else:
+                    print('How did you get here?')
 
-                    if association_class_name == 'bagcompartmenttoitemassociation':
-                        bag_compartment_to_item_association = BagCompartmentToItemAssociation.objects.get(pk=association_object_pk)
-                        item = bag_compartment_to_item_association.item
-                        print('%s in %s' % (item.name, bag_compartment_to_item_association.bag_compartment.name))
-                    elif association_class_name == 'kittoitemassociation':
-                        kit_to_item_association = KitToItemAssociation.objects.get(pk=association_object_pk)
-                        item = kit_to_item_association.item
-                        print('%s in %s' % (item.name, kit_to_item_association.kit.name))
-                    elif association_class_name == 'kitcompartmenttoitemassociation':
-                        kit_compartment_to_item_association = KitCompartmentToItemAssociation.objects.get(pk=association_object_pk)
-                        item = kit_compartment_to_item_association.item
-                        print('%s in %s' % (item.name, kit_compartment_to_item_association.kit_compartment.name))
-                    else:
-                        print('How did you get here?')
+        except ValueError:
+            pass
 
-            except ValueError:
-                pass
-
-        template = 'order-form.html'
-        context = {
-            'form': form,
-            'vehicle_bags': vehicle_bags,
-        }
-        return render(request, template, context)
+    template = 'order-form.html'
+    context = {
+        'vehicle_bags': vehicle_bags,
+    }
+    return render(request, template, context)
