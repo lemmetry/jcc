d2a3eed20a2112d53a17971059601ca815098138
lemmetry <lemmetry@users.noreply.github.com>
2021-01-29 17:57:49 -0500

    Issue 32 (#33)
    * Fixes #32.
    
    * Removed 'timestamp' from 'VehicleOrder'.

--- inventory/models.py
+++ inventory/models.py
@@ -221,14 +221,13 @@ class VehicleOrder(models.Model):
                                 blank=True,
                                 null=True,
                                 on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
     station_order = models.ForeignKey('StationOrder',
                                       blank=True,
                                       null=True,
                                       on_delete=models.CASCADE)
 
     def __str__(self):
-        return '%s: %s' % (self.vehicle.name, self.timestamp.strftime("%d %B, %Y at %H:%M:%S"))
+        return self.vehicle.name
 
 
 class VehicleOrderToItemAssociation(models.Model):
