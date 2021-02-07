cfc48d29db6c6c3c70a26db7c86b4b46e2dff4b3
Artem Kisel <artemkisel@yahoo.com>
2021-02-06 21:31:56 -0500

    Write the script that establishes connection with the remote server, stops the app, removes the app folder, clones the app from GitHub and restarts the app using local env variables.

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 
-stop_django_server() {
-  fuser -k 80/tcp
-  printf "... Django stopped.\n"
-}
+ssh -t "$VPS_USERNAME"@"$VPS_IP_ADDRESS" << EOF
+export JCC_EMAIL_HOST=$JCC_EMAIL_HOST
+export JCC_EMAIL_HOST_USER=$JCC_EMAIL_HOST_USER
+export JCC_SENDGRID_API_KEY=$JCC_SENDGRID_API_KEY
+export JCC_EMAIL_PORT=$JCC_EMAIL_JCC_EMAIL_PORT
+export JCC_EMAIL_USE_TLS=$JCC_EMAIL_USE_TLS
+export JCC_EMAIL_FROM=$JCC_EMAIL_FROM
+export JCC_EMAIL_TO=$JCC_EMAIL_TO
 
-remove_app() {
-  printf "\nDeleting '%s' folder...\n" "$1"
-  full_path="apps/$1/"
-  rm -rf "$full_path"
-  printf "... '%s' deleted.\n" "$1"
-}
+fuser -k 80/tcp
+printf "... Django stopped.\n"
 
-clone_repo() {
-  printf "\nCloning '%s' app from GitHub...\n" "$1"
-  cd apps/
-  clone_link="https://$2:$3@github.com/$2/$1.git"
-  git clone "$clone_link"
-  printf "... '%s' cloned.\n" "$1"
-}
+printf "\nDeleting $GITHUB_REPO_NAME folder...\n"
+rm -rf "apps/jcc/"
+printf "... $GITHUB_REPO_NAME deleted.\n"
 
-start_django_server() {
-  printf "\nStarting Django server...\n"
-  cd jcc
-  pipenv install
-  pipenv run ./manage.py runserver
-}
+printf "\nCloning $GITHUB_REPO_NAME app from GitHub..."
+cd apps/
+git clone "https://$GITHUB_USERNAME:$GITHUB_VPS_TOKEN@github.com/$GITHUB_USERNAME/$GITHUB_REPO_NAME.git"
+printf "... 'jcc' cloned.\n"
 
-vps_username=${VPS_USERNAME}
-vps_ip_address=${VPS_IP_ADDRESS}
-github_repo_name=${GITHUB_REPO_NAME}
-github_username=${GITHUB_USERNAME}
-github_vps_token=${GITHUB_VPS_TOKEN}
-
-ssh -t "$vps_username"@"$vps_ip_address" \
-"$(typeset -f); stop_django_server && \
-$(typeset -f); remove_app $github_repo_name && \
-$(typeset -f); clone_repo $github_repo_name $github_username $github_vps_token && \
-$(typeset -f); start_django_server"
+printf "\nStarting Django...\n"
+cd jcc/
+pipenv install
+pipenv run ./manage.py runserver routinemod.com:80
+EOF
