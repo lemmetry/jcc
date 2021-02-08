a08793ed99a74522fe08370f4040ebffd13fc915
lemmetry <lemmetry@users.noreply.github.com>
2021-02-07 21:40:15 -0500

    Fixes #48. (#49)
    * Fixes #48.
    
    * Use app secret key in production settings as env varibale.

--- jcc/settings/production.py
+++ jcc/settings/production.py
@@ -8,6 +8,6 @@ from .base import *
 SECRET_KEY = os.environ['JCC_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.routinemod.com']
