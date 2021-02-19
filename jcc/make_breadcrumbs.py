6679025e29561dbb13831d359f9f4b236866f42a
Artem Kisel <artemkisel@yahoo.com>
2021-02-19 17:00:35 -0500

    Reformat the code to improve readability.

--- jcc/make_breadcrumbs.py
+++ jcc/make_breadcrumbs.py
@@ -6,16 +6,22 @@ def make_home_breadcrumb():
 
 
 def make_station_breadcrumb(station):
-    return [station.get_name(), reverse('station orders dashboard',
-                                        kwargs={'station_id': station.station_id})
-            ]
+    return [
+        station.get_name(),
+        reverse('station orders dashboard', kwargs={
+            'station_id': station.station_id
+        })
+    ]
 
 
 def make_order_breadcrumb(station_order):
-    return ['Order #%s' % station_order.pk, reverse('make station order',
-                                                    kwargs={'station_id': station_order.station.station_id,
-                                                            'order_pk': station_order.pk})
-            ]
+    return [
+        'Order #%s' % station_order.pk,
+        reverse('make station order', kwargs={
+            'station_id': station_order.station.station_id,
+            'order_pk': station_order.pk
+        })
+    ]
 
 
 def make_vehicle_breadcrumb(vehicle):
