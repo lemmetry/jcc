03931537f6fff06f8259c47529cd7ca83198208b
Artem Kisel <artemkisel@yahoo.com>
2021-02-20 11:54:02 -0500

    Make breadcrumbs as dict to allow explicit recall of values in templates.

--- jcc/make_breadcrumbs.py
+++ jcc/make_breadcrumbs.py
@@ -2,27 +2,30 @@ from django.urls import reverse
 
 
 def make_home_breadcrumb():
-    return ['Home', reverse('home')]
+    return {
+        'text': 'Home',
+        'link': reverse('home')
+    }
 
 
 def make_station_breadcrumb(station):
-    return [
-        station.get_name(),
-        reverse('station orders dashboard', kwargs={
+    return {
+        'text': station.get_name(),
+        'link': reverse('station orders dashboard', kwargs={
             'station_id': station.station_id
         })
-    ]
+    }
 
 
 def make_order_breadcrumb(station_order):
-    return [
-        'Order #%s' % station_order.pk,
-        reverse('make station order', kwargs={
+    return {
+        'text': 'Order #%s' % station_order.pk,
+        'link': reverse('make station order', kwargs={
             'station_id': station_order.station.station_id,
             'order_pk': station_order.pk
         })
-    ]
+    }
 
 
 def make_vehicle_breadcrumb(vehicle):
-    return [vehicle.name, '']
+    return {'text': vehicle.name}
