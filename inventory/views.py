0a39addd60b4db9fb58d23d3843f5a5d17045c4a
Artem Kisel <artemkisel@yahoo.com>
2021-02-25 15:02:09 -0500

    Use local time, all interested parties are in the same locality.

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 
 from django.contrib.auth.decorators import login_required
 from django.core.mail import EmailMultiAlternatives
--- jcc/settings/base.py
+++ jcc/settings/base.py
@@ -101,13 +101,13 @@ LOGIN_URL = 'user sign in'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
