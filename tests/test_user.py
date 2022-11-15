31936553e2296f3455376264b7e23385f42ef346
lemmetry <lemmetry@users.noreply.github.com>
2022-11-15 14:11:44 -0500

    WIP: Working on authentication user with a cookie. (#107)
    * WIP: Working on authentication user with a cookie.
    
    * WIP: Add setup-teardown mechanism for getting authentication cookie.
    
    * Delete all cookies in open browser prior to yielding valid cookie to the test scenario, otherwise user remaines signed in.
    
    * WIP: Add authenticated_browser fixture for tests that require signed-in user.
    
    * WIP: Check that there are all 6 stations on the StationsPage.

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,8 +3,8 @@ from pages.stations import StationsPage
 from pages.accounts import TestAccount
 
 
-def test_user_can_sign_in(browser):
-    sign_in_page = SignInPage(browser)
+def test_user_can_sign_in(default_browser):
+    sign_in_page = SignInPage(default_browser)
     sign_in_page.load()
 
     username = TestAccount.username
@@ -13,14 +13,14 @@ def test_user_can_sign_in(browser):
     sign_in_page.sign_in(username=username,
                          password=password)
 
-    stations_page = StationsPage(browser)
+    stations_page = StationsPage(default_browser)
     stations_page_title = stations_page.get_page_title()
 
     assert stations_page_title == 'Stations'
 
 
-def test_user_cant_sign_in_with_invalid_password(browser):
-    sign_in_page = SignInPage(browser)
+def test_user_cant_sign_in_with_invalid_password(default_browser):
+    sign_in_page = SignInPage(default_browser)
     sign_in_page.load()
 
     username = TestAccount.username
@@ -30,9 +30,9 @@ def test_user_cant_sign_in_with_invalid_password(browser):
                          password=invalid_password)
 
     sign_in_page_url = sign_in_page.url
-    assert browser.current_url == sign_in_page_url
+    assert default_browser.current_url == sign_in_page_url
 
-    stations_page = StationsPage(browser)
+    stations_page = StationsPage(default_browser)
     stations_page.load()
 
     station_page_title = stations_page.get_page_title()
@@ -42,4 +42,21 @@ def test_user_cant_sign_in_with_invalid_password(browser):
     assert welcome_user_message is None
 
     redirect_to_sign_in_url = '%s?next=%s' % (sign_in_page_url, StationsPage.PATH)
-    assert browser.current_url == redirect_to_sign_in_url
+    assert default_browser.current_url == redirect_to_sign_in_url
+
+
+def test_signed_in_user_can_open_stations_page(authenticated_browser):
+    stations_page = StationsPage(authenticated_browser)
+    stations_page.load()
+
+    stations_page_title = stations_page.get_page_title()
+    assert stations_page_title == 'Stations'
+
+    welcome_user_message = stations_page.get_welcome_user_message()
+    assert 'welcome, ' in welcome_user_message
+
+    # TODO - replace hardcoded names below with actual data from the db
+    stations_names_in_db = ['Station 1', 'Station 2', 'Station 3', 'Station 4', 'Station 5', 'Station 6']
+
+    stations_names_on_the_page = stations_page.get_stations_names()
+    assert stations_names_in_db == stations_names_on_the_page
